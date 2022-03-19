89820fc0d659bb0a89428c1eb6129f781ad08dc4
ILoveAstesia <1324677263@qq.com>
2022-03-19 20:20:16 +0800

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,58 @@
+var navbor=document.getElementById("navbar");
+		var menu=document.getElementById("menu");
+		window.onscroll=function(){
+			if(window.pageYOffset >= menu.offsetTop){
+				navbor.classList.add("sticky");
+			}
+			else{
+				navbor.classList.remove("sticky");
+			}
+		
+		}
+		window.onload=function(){
+			document.getElementById("submenu1").hidden=true;
+			document.getElementById("submenu2").hidden=true;
+			document.getElementById("submenu3").hidden=true;
+		}
+		function drop_down1(){
+			if(document.getElementById("submenu1").hidden===true)
+				document.getElementById("submenu1").hidden=false;
+				else
+				document.getElementById("submenu1").hidden=true;
+		}
+		function drop_down2(){
+			if(document.getElementById("submenu2").hidden===true)
+				document.getElementById("submenu2").hidden=false;
+				else
+				document.getElementById("submenu2").hidden=true;
+		}
+		function drop_down3(){
+			if(document.getElementById("submenu3").hidden===true)
+				document.getElementById("submenu3").hidden=false;
+				else
+				document.getElementById("submenu3").hidden=true;
+		}
+		function checkpassword(){
+			var password=document.getElementById('password').value;
+			var repassword=document.getElementById('repassword').value;
+			if(password==repassword){
+				
+				return true;
+			}
+			else if(password!=repassword){
+				alert("密码不一致请重新输入")
+				return false;
+			}
+		}
+		function checkpassword2(){
+			var password=document.getElementById('password2').value;
+			var repassword=document.getElementById('repassword2').value;
+			if(password==repassword){
+				
+				return true;
+			}
+			else if(password!=repassword){
+				alert("密码不一致请重新输入")
+				return false;
+			}
+		}
