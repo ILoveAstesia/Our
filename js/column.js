7364433c8892ce1b28229237840a400a23299003
ILoveAstesia <1324677263@qq.com>
2022-03-20 11:38:52 +0800

    合并

---
+++ js/column.js
@@ -0,0 +1,35 @@
+/*
+window.onload=function(){
+    document.getElementById("submenu1").hidden=true;
+}
+    function drop_down1(){
+    if(document.getElementById("submenu1").hidden===true)
+        document.getElementById("submenu1").hidden=false;
+    else
+        document.getElementById("submenu1").hidden=true;
+}
+
+sfHover = function()
+{
+    var sfEls = document.getElementById("column").getElementsByTagName("LI");
+
+    for(var i=0;i<sfEls.length;i++)
+    {
+        sfEls[i].onmouseover=function()
+        {
+            this.calssName+="sfhover";
+        }
+
+        sfEls[i].onmouseout=function()
+        {
+            this.calssName=this.calssName.replace(new RegExp(" sfhover\\b"),"");
+        }
+    }
+    
+}
+
+if(window.attachEvent)window.attachEvent("onload",sfHover);
+
+*/
+
+
